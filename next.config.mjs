17b5697a8a9ec3e7e941394ccaa191a0ad14b66c
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-12-05 11:28:59 +0900

    next.config.js 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@fullcalendar'],
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+      domains: ['dapi.kakao.com', 'k.kakaocdn.net']
+    },
+    async headers() {
+      return [
+          {
+              source: '/:path*',
+              headers: [
+                  {
+                      key: 'Content-Security-Policy',
+                      value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' dapi.kakao.com k.kakaocdn.net;"
+                  }
+              ]
+          }
+      ]
+    }
   }
   
   export default nextConfig;
