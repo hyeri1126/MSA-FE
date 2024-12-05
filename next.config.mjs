49f84658afbe19ecb9f1a9e80154e3d12a4c1612
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-12-05 11:47:12 +0900

    config 파일 재수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,6 @@
 const nextConfig = {
     transpilePackages: ['@fullcalendar'],
     reactStrictMode: false,
-    images: {
-      domains: ['dapi.kakao.com', 'k.kakaocdn.net']
-    },
-    async headers() {
-      return [
-          {
-              source: '/:path*',
-              headers: [
-                  {
-                      key: 'Content-Security-Policy',
-                      value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' dapi.kakao.com k.kakaocdn.net;"
-                  }
-              ]
-          }
-      ]
-    }
   }
   
   export default nextConfig;
